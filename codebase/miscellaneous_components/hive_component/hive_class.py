fb68750ea56fbaa33d88ec8f60a3b77a15b7f7a6
J P C <homeserverstatus@gmail.com>
2018-12-20 22:05:54 +0000

    Getting initial device data

--- codebase/miscellaneous_components/hive_component/hive_class.py
+++ codebase/miscellaneous_components/hive_component/hive_class.py
@@ -2,11 +2,12 @@ from ...common_components.datetime_datatypes import datetime_module as DateTime
 from .hivecredentials_subcomponent import hivecredentials_module as Credentials
 #from .webscraper_subcomponent import webscraper_module as WebScraper
 #from .astroitem_subcomponent import astroitem_module as AstroItem
-from urllib.parse import urlencode as DataEncoder
+#from urllib.parse import urlencode as DataEncoder
 from urllib.request import urlopen as GetWebPage
 from urllib.request import Request as GenerateWebRequest
-from urllib.request import URLError as WebError
-
+#from urllib.request import URLError as WebError
+from json import loads as DecodeJson
+from .hivewebscraper_subcomponent import hivewebscraper_privatefunctions as HiveFunctions
 
 
 class DefineHiveInterface:
@@ -23,11 +24,35 @@ class DefineHiveInterface:
 
 		self.urlendpointroot = "https://api-prod.bgchprod.info:443/omnia"
 
-		testeroutput = self.interactwithhive(self.body, "/auth/sessions")
+		self.loginsessiondata = self.interactwithhive("/auth/sessions", self.body, "", "sessions")
+		self.loginsessiondata = self.loginsessiondata[0]
 
-		print("\\/=======================================\\/")
-		print(testeroutput.read())
-		print("/\\=======================================/\\")
+		self.devicenodedata = self.interactwithhive("/nodes", "", self.loginsessiondata['sessionId'], "nodes")
+
+
+		for node in self.devicenodedata:
+			nodename = node['name']
+			print("---------------------------------")
+			print(nodename)
+			deviceattributes = node['attributes']
+			rawdevicetype = HiveFunctions.getvalue(deviceattributes['nodeType'])
+			if rawdevicetype == 'http://alertme.com/schema/json/node.class.thermostatui.json#':
+				devicetype = "Thermostat"
+			elif rawdevicetype == 'http://alertme.com/schema/json/node.class.hub.json#':
+				devicetype = "Home Hub"
+			elif rawdevicetype == 'http://alertme.com/schema/json/node.class.thermostat.json#':
+				devicetype = "Boiler Switch"
+			else:
+				devicetype = "OTHER"
+			if 'powerSupply' in deviceattributes:
+				print(devicetype, "Device last seen: ", HiveFunctions.sanitiseunixdate(node['lastSeen']))
+				powersupply = deviceattributes['powerSupply']
+				if HiveFunctions.getvalue(powersupply) == "BATTERY":
+					batterystate = deviceattributes['batteryVoltage']
+					print(devicetype, "Device = battery: ", HiveFunctions.getvalue(batterystate), HiveFunctions.getpolltime(batterystate))
+				else:
+					print(devicetype, "Device = mains")
+
 
 
 		#self.webscraper = WebScraper.createscraper(locationname, longitude, latitude, timeshift, connectionmode)
@@ -83,14 +108,29 @@ class DefineHiveInterface:
 	# 	return outcome
 
 
-	def interactwithhive(self, requestdata, endpoint):
+	def interactwithhive(self, endpoint, requestdata, sessionid, rootnodename):
 
 		fullurlendpoint = self.urlendpointroot + endpoint
 
-		webrequest = GenerateWebRequest(fullurlendpoint, data=requestdata.encode('ascii', 'ignore'))
+		if requestdata == "":
+			webrequest = GenerateWebRequest(fullurlendpoint)
+		else:
+			webrequest = GenerateWebRequest(fullurlendpoint, data=requestdata.encode('ascii', 'ignore'))
 		webrequest.add_header('Content-Type', 'application/vnd.alertme.zoo-6.1+json')
 		webrequest.add_header('Accept', 'application/vnd.alertme.zoo-6.1+json')
 		webrequest.add_header('X-Omnia-Client', 'Hive Web Dashboard')
+		if sessionid != "":
+			webrequest.add_header('X-Omnia-Access-Token', sessionid)
 		outcome = GetWebPage(webrequest)
+		outcome = outcome.read()
+		outcome = outcome.decode('utf-8', 'ignore')
+		outcome = DecodeJson(outcome)
+		outcome = outcome[rootnodename]
+
+		print("\\/=======================================\\/")
+		print(fullurlendpoint)
+		print("===========================================")
+		print(outcome)
+		print("/\\=======================================/\\")
 
 		return outcome
--- codebase/miscellaneous_components/hive_component/hivewebscraper_subcomponent/hivewebscraper_privatefunctions.py
+++ codebase/miscellaneous_components/hive_component/hivewebscraper_subcomponent/hivewebscraper_privatefunctions.py
@@ -1,110 +1,23 @@
-from ....common_components.datetime_datatypes import clock_module as Clock
+import time as TimeFunctions
 
+def sanitiseunixdate(integervalue):
 
-def preparelocation(decimallocation):
-	if decimallocation < 0.0:
-		sign = -1
-	else:
-		sign = 1
-
-	location = abs(decimallocation)
-
-	degs = int(location)
-
-	mins = int((location - float(degs)) * 60.0)
-
-	return sign, degs, mins
+	return TimeFunctions.strftime('%Y-%m-%d %H:%M:%S', TimeFunctions.localtime(integervalue / 1000))
 
 
 
-def sanitisetime(textstring, defaultmode):
+def getvalue(dictionary):
 
-	hour = textstring[0:2]
-	min = textstring[2:4]
-
-	try:
-		hourvalue = int(hour)
-		minvalue = int(min)
-		outcometime = Clock.createastime(hourvalue, minvalue, 0)
-		outcomevalidity = True
-	except:
-		outcomevalidity = False
-		if defaultmode == "Start":
-			outcometime = Clock.createastime(0, 0, 0)
-		else:
-			outcometime = Clock.createastime(23, 59, 59)
-		#print("Problems reading sunrise/sunset time: ", textstring)
-
-	return outcometime, outcomevalidity
+	return dictionary['displayValue']
 
 
 
-def generatedummydata(datamode):
+def getpolltime(dictionary):
 
-	outcome = "Dummy Data"
-
-	if datamode == "Day":
-		hourstart = 10
-		hourend = 13
-	elif datamode == "Civ":
-		hourstart = 7
-		hourend = 16
-	elif datamode == "Nau":
-		hourstart = 4
-		hourend = 19
-	else:
-		hourstart = 1
-		hourend = 22
-
-	for day in range(1, 32):
-
-		starttime = Clock.createastime(hourstart, 30, 0)
-		endtime = Clock.createastime(hourend, 30, 0)
-
-		starttimestring = starttime.getsecondlesstext()
-		endtimestring = endtime.getsecondlesstext()
-
-		timestring = "  " + starttimestring[:2] + starttimestring[-2:] + " " + endtimestring[:2] + endtimestring[-2:]
-
-		daytext = "00" + str(day)
-		outcome = outcome + "\n" + daytext[-2:]
-
-		for month in range(1, 13):
-			outcome = outcome + timestring
-
-	outcome = outcome + "\nEnd of dummy data"
-
-	return outcome
+	return sanitiseunixdate(dictionary['reportReceivedTime'])
 
 
 
-def getindexes(lookupday, lookupmonth):
+def getchangetime(dictionary):
 
-	desiredlinestart = str(lookupday) + "  "
-	if lookupday < 10:
-		desiredlinestart = "0" + desiredlinestart
-
-	desiredcolumn = (lookupmonth * 11) - 7
-
-	return desiredlinestart, desiredcolumn
-
-
-
-def extracttimings(dataline, columnindex):
-
-	fulldata = dataline[(columnindex + 0):(columnindex + 9)]
-	invalidrowone = "         "
-	invalidrowtwo = "==== ===="
-	if (fulldata == invalidrowone) or (fulldata == invalidrowtwo):
-		rowvalidity = False
-	else:
-		rowvalidity = True
-
-	starttext = dataline[(columnindex + 0):(columnindex + 4)]
-	endtext = dataline[(columnindex + 5):(columnindex + 9)]
-
-	starttime, startvalidity = sanitisetime(starttext, "Start")
-	endtime, endvalidity = sanitisetime(endtext, "End")
-
-	return rowvalidity, starttime, startvalidity, endtime, endvalidity
-
+	return sanitiseunixdate(dictionary['reportChangedTime'])
